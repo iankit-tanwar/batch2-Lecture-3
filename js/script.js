2ffe8b7200505dbeb0e32e4e8cf94cd1030f50e9
iankit-tanwar <tankitofficial01.gmail.com>
2023-06-15 14:29:24 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+
+
+console.log('hello form the external file');
---
+++ js/test.js
@@ -0,0 +1,55 @@
+
+
+
+//1 variable decleration
+var x; //statement 1
+
+//2 variable initialization
+var y ;   //statement 2
+y =10;    //statement 3
+
+
+
+//3 variable decleration and initialization
+
+var z =70;   //statement 4
+
+console.log( y+z);
+
+
+
+
+
+var c = 5;
+
+var d = c++; // post Increment operator
+
+console.log("C-->",c);
+console.log("D-->",d);
+
+
+var e = 5;
+
+var f = ++e; // pre Increment operator
+
+console.log("E-->",e);
+console.log("F-->",f);
+
+
+
+var g = 1;
+
+
+//Addition Assignment Operator
+g += 1; // right to left
+console.log("g--->",g);
+
+
+var h = 5;
+//Subtraction Assignment Operator
+h -= 1; // right to left
+console.log("h--->",h);
+
+
+
+
